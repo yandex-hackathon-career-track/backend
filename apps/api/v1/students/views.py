d1c22dc80c55a9ff33886f10162f626db2e89b9b
Philipp Pyhonin <caveinfix@gmail.com>
2023-10-27 19:04:29 +0300

    add serializer

--- apps/api/v1/students/views.py
+++ apps/api/v1/students/views.py
@@ -1,13 +1,14 @@
 from rest_framework import viewsets
 
 from apps.students.selectors import get_all_applicants
-from apps.api.v1.students.serializers import ApplicantSerializer, ApplicantsListSerializer
-from apps.api.v1.permissions import IsApplicantOrReadOnly
+from apps.api.v1.students.serializers import (
+    ApplicantSerializer,
+    ApplicantsListSerializer,
+)
 
 
 class ApplicantViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = get_all_applicants()
-    permission_classes = [IsApplicantOrReadOnly]
 
     def get_serializer_class(self):
         if self.action == "list":
